fa1382e1e7c367ae213a788277a78c88e5ebfbb1
Rogerxt9k <81235122+Rogerxt9k@users.noreply.github.com>
2023-12-07 19:49:01 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -36,8 +36,8 @@ async def start(client, message):
     button = InlineKeyboardMarkup([[
         InlineKeyboardButton("👨‍💻 Dᴇᴠꜱ 👨‍💻", callback_data='dev')
         ],[
-        InlineKeyboardButton('📯 Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/funkomovies'),
-        InlineKeyboardButton('💁‍♂️ Channel', url='https://t.me/funkomovies')
+        InlineKeyboardButton('📯 🆄🅿🅳🅰🆃🅴🆂', url='https://t.me/funkomovies'),
+        InlineKeyboardButton('💁‍♂️ 🅲🅷🅰🅽🅽🅴🅻', url='https://t.me/funkomovies')
         ],[
         InlineKeyboardButton('🎛️ Aʙᴏᴜᴛ', callback_data='about'),
         InlineKeyboardButton('🛠️ Hᴇʟᴩ', callback_data='help')
@@ -58,8 +58,8 @@ async def cb_handler(client, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup([[
                 InlineKeyboardButton("👨‍💻 Dᴇᴠꜱ 👨‍💻", callback_data='dev')
                 ],[
-                InlineKeyboardButton('📯 Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/funkomovies'),
-                InlineKeyboardButton('💁‍♂️ Channel', url='https://t.me/funkomovies')
+                InlineKeyboardButton('📯 🆄🅿🅳🅰🆃🅴🆂', url='https://t.me/funkomovies'),
+                InlineKeyboardButton('💁‍♂️ 🅲🅷🅰🅽🅽🅴🅻', url='https://t.me/funkomovies')
                 ],[
                 InlineKeyboardButton('🎛️ Aʙᴏᴜᴛ', callback_data='about'),
                 InlineKeyboardButton('🛠️ Hᴇʟᴩ', callback_data='help')
